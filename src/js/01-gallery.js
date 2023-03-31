2d1381a3a5b38a87cfc4a9b626a0fa24d280233a
kristinaordenans <okristin92@gmail.com>
2023-03-31 10:21:00 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems){ 
+    return galleryItems.map(({preview, original, description}) => {
+        return `<li class = "gallery__item"><a class="gallery__link" href = "${original}" target = "_self"><img class = "gallery__image" src="${preview}" alt = "${description}"/></a></li>`;
+    }).join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+ });
+
+console.log(galleryItems);
 console.log(galleryItems);
