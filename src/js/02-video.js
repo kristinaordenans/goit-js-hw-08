7c9cbae5468bda39a3a92a2bee0350164ff1f211
kristinaordenans <okristin92@gmail.com>
2023-04-01 11:18:41 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.getVideoTitle().then((title) => {
+    console.log('title:', title);
+});
+
+const timeStorage = "videoplayer-current-time";
+const currentTime = localStorage.getItem(timeStorage);
+
+player.setCurrentTime(currentTime);
+
+function getCurrentTime(e) {
+    localStorage.setItem(timeStorage, e.seconds)
+}
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
