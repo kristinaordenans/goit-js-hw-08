96573b135850aa88c7322ab5eb88bd79ef1301f1
kristinaordenans <okristin92@gmail.com>
2023-04-02 17:27:12 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,21 +4,15 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.getVideoTitle().then((title) => {
-    console.log('title:', title);
-});
-
 const timeStorage = "videoplayer-current-time";
 const currentTime = localStorage.getItem(timeStorage);
     
-if (currentTime !== 0) {
+if (currentTime !== null) {
     player.setCurrentTime(currentTime);
-};
-
-
-function getCurrentTime(e) {
-    localStorage.setItem(timeStorage, e.seconds)
 };
 
 player.on('timeupdate', throttle(getCurrentTime, 1000));
 
+function getCurrentTime(e) {
+    localStorage.setItem(timeStorage, e.seconds)
+};
