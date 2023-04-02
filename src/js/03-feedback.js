596ec9dea9ea2ce3ca9670177908b2ca81fbcc9d
kristinaordenans <okristin92@gmail.com>
2023-04-02 14:25:41 +0300

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,37 +2,75 @@ import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
 const FEEDBACK_KAY = "feedback-form-state";
-const feedbackData = {};
 
-const emailInputEl = document.querySelector('input[type="email"]');
-const messageInputEl = document.querySelector('textarea[name="message"]');
-const submitBtn = document.querySelector('button[type="submit"]');
-submitBtn.setAttribute("disabled", ``)
+let feedbackData = {};
 
-formEl.addEventListener('input', throttle(setInputData, 500));
-formEl.addEventListener('submit', removeInputData);
+const emailInputEl = document.querySelector('.feedback-form iput');
+const messageInputEl = document.querySelector('.feedback-form textarea');
 
 
-function activeButton() {
+formEl.addEventListener('input', throttle(e => {
+    feedbackData[e.target.name] = e.target.value;
+    localStorage.setItem(FEEDBACK_KAY, JSON.stringify(feedbackData));
+  }, 500)
+);
+
+
+formEl.addEventListener('submit', e => {
+    e.preventDefault();
     if (emailInputEl.value !== '' && messageInputEl.value !== '') {
-        submitBtn.removeAttribute("disabled");
+        console.log(feedbackData);
+        localStorage.removeItem(FEEDBACK_KAY);
+        e.currentTarget.reset();
+        return;
+    }
+    alert('All fields');
+});
+
+function fillForm() {
+    const savedEl = localStorage.getItem(FEEDBACK_KAY);
+    if (savedEl) {
+        feedbackData = JSON.parse(savedEl);
+        emailInputEl.value = feedbackData.email || '';
+        messageInputEl.value = feedbackData.message || '';
     }
 };
 
 
-function setInputData(e) {
-    e.preventDefault()
-    feedbackData[e.target.name] = e.target.value;
-    localStorage.setItem('FEEDBACK_KAY', JSON.stringify(feedbackData));
-    activeButton();
-};
+// formEl.addEventListener('input', throttle(setInputData, 500));
+// formEl.addEventListener('submit', removeInputData);
 
 
-function removeInputData(e) {
-    e.preventDefault()
-    console.log(feedbackData);
-    localStorage.removeItem(FEEDBACK_KAY);
-    e.target.reset();
-    submitBtn.setAttribute("disabled",``);
-};
+// const dataLocalStorage = localStorage.getItem(FEEDBACK_KAY);
+// const parseDataLocalStorage = JSON.parse(dataLocalStorage);
+
+
+
+
+
+// const submitBtn = document.querySelector('button[type="submit"]');
+// submitBtn.setAttribute("disabled", ``);
+
+// function activeButton() {
+//     if (emailInputEl.value !== '' && messageInputEl.value !== '') {
+//         submitBtn.removeAttribute("disabled");
+//         localStorage.removeItem(FEEDBACK_KAY);
+        
+//     }
+// };
+
+// function setInputData(e) {
+//     e.preventDefault()
+//     feedbackData[e.target.name] = e.target.value;
+//     localStorage.setItem('FEEDBACK_KAY', JSON.stringify(feedbackData));
+//     activeButton();
+// };
+
+// function removeInputData(e) {
+//     e.preventDefault()
+//     console.log(feedbackData);
+//     localStorage.removeItem(FEEDBACK_KAY);
+//     e.target.reset();
+//     submitBtn.setAttribute("disabled",``);
+// };
 
